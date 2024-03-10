64449a0578a35a357b288b94e507511f8296c711
leumars-o <samex150@gmail.com>
2024-03-10 18:53:08 +0100

    save to json and reload methods modified

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,7 @@ classes:
 """
 import uuid
 import datetime
+# from models import storage
 import models
 
 
@@ -59,3 +60,16 @@ class BaseModel():
         model_dict['created_at'] = model_dict['created_at'].isoformat()
         model_dict['updated_at'] = model_dict['updated_at'].isoformat()
         return (model_dict)
+
+all_objs = storage.all()
+print("-- Reloaded objects --")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj)
+
+print("-- Create a new object --")
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+my_model.save()
+print(my_model)
